2c324762f00855c1665c02ceceaf5387474b18a5
Sebastián Schuchhardt <seba.sch999@gmail.com>
2016-08-12 18:56:14 -0400

    fix vote

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ angular
 })
 .factory('Candidate', ['$firebase', 'CONFIG', function ($firebase, CONFIG) {
 
-		return function(name) {
+		return function(name, owner) {
 			var self = this;
 			var fb =  new Firebase(CONFIG.firebaseId);
 			var entry = fb.child('/votes/' + name);
@@ -18,8 +18,10 @@ angular
 	  
 			self.name = name;
 			self.votes = sync.$asArray();
+			console.log(self)
 	 
 			self.vote = function() {
+				console.log("bote")
 				self.votes.$add(1);				
 			};
 		};
@@ -27,11 +29,18 @@ angular
 .controller('MainCtrl', function ($scope, Candidate, CONFIG, $timeout) {
 		var ref = new Firebase(CONFIG.firebaseId);
 		var projects = ref.child("candidates")
+		// var votes = ref.child('votes');
+
 		$scope.newCandidate = {};
 
 		projects.on("value", function(snapshot) {
   			$timeout(function() {
-  				$scope.candidates = snapshot.val()	
+  				$scope.candidates = snapshot.val()
+  				// var cand = snapshot.val()
+  				// console.log(cand)
+  				// angular.forEach(cand, function(k, c){
+  				// 	$scope.candidates = new Candidate(c.name, c.owner),
+  				// })	
   			}, 100);  			
 		});
 
@@ -51,6 +60,18 @@ angular
 		$scope.removeCandidate = function(pId){
 			projects.child(pId).remove();
 		}
+
+		$scope.voteCandidate = function (id, candidate){
+			console.log(candidate)
+			console.log(id)
+			if (angular.isDefined(candidate.votes)) {
+				candidate.votes.push(1)		
+			} else{
+				candidate.votes = [1];
+			}
+			projects.child(id).update({votes: candidate.votes})
+		}
+		
 
 
   });
