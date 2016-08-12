305fa0cc9bc78c29f0d67e5531dc61a1dc40b6a9
Sebastián Schuchhardt <seba.sch999@gmail.com>
2016-08-12 18:35:35 -0400

    Agregar/eliminar proyecto `

--- js/index.js
+++ js/index.js
@@ -3,14 +3,16 @@
 /**
  * Service - Voting Candidate
  */
-angular.module('firebaseVotingApp', ['firebase']);
-
-angular.module('firebaseVotingApp')
-  .factory('Candidate', ['$firebase', function ($firebase) {
+angular
+.module('firebaseVotingApp', ['firebase'])
+.constant('CONFIG', {
+    firebaseId: "https://hackaton-2brains.firebaseio.com/"
+})
+.factory('Candidate', ['$firebase', 'CONFIG', function ($firebase, CONFIG) {
 
 		return function(name) {
 			var self = this;
-			var fb =  new Firebase('https://hackaton-2brains.firebaseio.com/');
+			var fb =  new Firebase(CONFIG.firebaseId);
 			var entry = fb.child('/votes/' + name);
 			var sync = $firebase(entry);
 	  
@@ -21,17 +23,34 @@ angular.module('firebaseVotingApp')
 				self.votes.$add(1);				
 			};
 		};
-}]);
+}])
+.controller('MainCtrl', function ($scope, Candidate, CONFIG, $timeout) {
+		var ref = new Firebase(CONFIG.firebaseId);
+		var projects = ref.child("candidates")
+		$scope.newCandidate = {};
 
-/**
- * Controller
- */
+		projects.on("value", function(snapshot) {
+  			$timeout(function() {
+  				$scope.candidates = snapshot.val()	
+  			}, 100);  			
+		});
 
-angular.module('firebaseVotingApp')
-  .controller('MainCtrl', function ($scope, Candidate) {
-		$scope.candidates = [
-			new Candidate('Un red social para perros (por: El Chico Terry)'),
-			new Candidate('Cocinar sin sal (por: Martín Carcamo)'),
-			new Candidate('Tomarse un gimnasio en Pokemon (por: Fyto Manga)')
-		];
+		// $scope.candidates = [
+		// 	new Candidate('Un red social para perros (por: El Chico Terry)'),
+		// 	new Candidate('Cocinar sin sal (por: Martín Carcamo)'),
+		// 	new Candidate('Tomarse un gimnasio en Pokemon (por: Fyto Manga)')
+		// ];
+
+		$scope.createCandidate = function (c, form){
+			form.$setPristine();
+			$scope.newCandidate = {};
+   			ref.child("candidates").push(c)
+
+		}
+
+		$scope.removeCandidate = function(pId){
+			projects.child(pId).remove();
+		}
+
+
   });
